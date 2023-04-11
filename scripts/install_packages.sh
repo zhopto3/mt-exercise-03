49490da796fccdff75299aebfab9a10fed5d150b
Zachary William Hopton <zacharywilliam.hopton@uzh.ch>
2023-04-11 14:51:28 +0200

    Removed prior edits

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -10,8 +10,6 @@ echo "Make sure this script is executed AFTER you have activated a virtualenv"
 
 pip install numpy torch sacremoses nltk
 
-#Need to install dataframe_image if the visualize_ppy.py script is to be used.
-pip install dataframe_image
 
 # install Moses scripts for preprocessing
 
