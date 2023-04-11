37216e9af62190b19ae451abe58e1a74e8412e22
Zachary William Hopton <zacharywilliam.hopton@uzh.ch>
2023-04-11 10:36:53 +0200

    Updated to sample the model trained on Shakespeare

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -15,8 +15,8 @@ device=""
 
 (cd $tools/pytorch-examples/word_language_model &&
     CUDA_VISIBLE_DEVICES=$device OMP_NUM_THREADS=$num_threads python generate.py \
-        --data $data/grimm \
+        --data $data/shake \
         --words 100 \
         --checkpoint $models/model.pt \
-        --outf $samples/sample
+        --outf $samples/sample1
 )
