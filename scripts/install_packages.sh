83e526975ebc215583fb75708ab7d1fced298db7
Zachary William Hopton <zacharywilliam.hopton@uzh.ch>
2023-04-11 14:41:52 +0200

    Add line to install other necessary py packages

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -10,6 +10,9 @@ echo "Make sure this script is executed AFTER you have activated a virtualenv"
 
 pip install numpy torch sacremoses nltk
 
+#Need to install dataframe_image if the visualize_ppy.py script is to be used.
+pip install dataframe_image
+
 # install Moses scripts for preprocessing
 
 git clone https://github.com/bricksdont/moses-scripts $tools/moses-scripts
