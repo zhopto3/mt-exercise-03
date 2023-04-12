107c6ed8f3a366061b9d52f6a1bb5bd3d00b09ee
Zachary William Hopton <zacharywilliam.hopton@uzh.ch>
2023-04-12 15:39:06 +0200

    Generates text with best and worst  models

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,3 +1,4 @@
+
 #! /bin/bash
 
 scripts=$(dirname "$0")
@@ -13,10 +14,27 @@ mkdir -p $samples
 num_threads=4
 device=""
 
+#uncomment below to get the sample data for the baseline model
+#(cd $tools/pytorch-examples/word_language_model &&
+#    CUDA_VISIBLE_DEVICES=$device OMP_NUM_THREADS=$num_threads python generate.py \
+#        --data $data/shake \
+#        --words 100 \
+#        --checkpoint $models/model.pt \
+#        --outf $samples/sample1
+#)
+
 (cd $tools/pytorch-examples/word_language_model &&
     CUDA_VISIBLE_DEVICES=$device OMP_NUM_THREADS=$num_threads python generate.py \
         --data $data/shake \
         --words 100 \
-        --checkpoint $models/model.pt \
-        --outf $samples/sample1
+        --checkpoint $models/model_0dropout.pt \
+        --outf $samples/samp_highestPpy
+)
+
+(cd $tools/pytorch-examples/word_language_model &&
+    CUDA_VISIBLE_DEVICES=$device OMP_NUM_THREADS=$num_threads python generate.py \
+        --data $data/shake \
+        --words 100 \
+        --checkpoint $models/model_point4dropout.pt \
+        --outf $samples/samp_lowestPpy
 )
